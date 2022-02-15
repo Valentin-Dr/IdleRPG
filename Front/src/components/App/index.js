85d92269796d7be9184220c76d0db8da2f6d314b
Castellan-Crowe <frederic.lavialle10@gmail.com>
2022-02-15 15:13:40 +0100

    create component success

--- Front/src/components/App/index.js
+++ Front/src/components/App/index.js
@@ -1,23 +1,22 @@
-import { Route, Routes } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
-import { useEffect } from "react";
-import Craft from "src/components/Craft";
-import Fight from "src/components/Fight";
-import NotFound from "src/components/NotFound";
-import NotAuthorize from 'src/components/NotAuthorize'
-import Home from "src/components/Home";
-import Menu from "../Menu";
-import Header from "../Header";
-import Shop from "../Shop";
-import Fishing from "../Fishing";
-import Mining from "../Mining";
-
-
-import "./style.scss";
-import Inventory from "../Character";
-import BtnDark from "../BtnDark/BtnDark";
+import { Route, Routes } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import Craft from 'src/components/Craft';
+import Fight from 'src/components/Fight';
+import NotFound from 'src/components/NotFound';
+import NotAuthorize from 'src/components/NotAuthorize';
+import Home from 'src/components/Home';
 import { checkUser } from 'src/actions/user';
+import Menu from '../Menu';
+import Header from '../Header';
+import Shop from '../Shop';
+import Fishing from '../Fishing';
+import Mining from '../Mining';
+import Success from '../Success';
 
+import './style.scss';
+import Inventory from '../Character';
+import BtnDark from '../BtnDark/BtnDark';
 
 // == Composant
 const App = () => {
@@ -28,23 +27,21 @@ const App = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-      dispatch(checkUser());
+    dispatch(checkUser());
   }, []);
 
   return (
-    <div className={darkMode ? "app dark" : "app"}>
+    <div className={darkMode ? 'app dark' : 'app'}>
 
-      <BtnDark/>
-      
-      
-      <Header/>
+      <BtnDark />
+      <Header />
       <Menu />
-      <div className="background-bg"></div>
+      <div className="background-bg" />
       {/* <h1>Jeu IDLE</h1> */}
       <Routes>
         <Route exact path="/" element={<Home />} />
-        { logged && 
-        (
+        { logged
+        && (
         <>
           <Route path="/shop" element={<Shop />} />
           <Route exact path="/inventory" element={<Inventory />} />
@@ -52,26 +49,26 @@ const App = () => {
           <Route path="/craft" element={<Craft />} />
           <Route path="/jobs/mining" element={<Mining job="mining" />} />
           <Route path="/jobs/fishing" element={<Fishing job="fishing" />} />
+          <Route path="/success" element={<Success />} />
         </>
-        )
-        }
-        { !logged &&
-         (
-          <>
-            <Route path="/shop" element={<NotAuthorize />} />
-            <Route exact path="/inventory" element={<NotAuthorize />} />
-            <Route exact path="/fighting" element={<NotAuthorize />} />
-            <Route path="/craft" element={<NotAuthorize />} />
-            <Route path="/jobs/mining" element={<NotAuthorize job="mining" />} />
-            <Route path="/jobs/fishing" element={<NotAuthorize job="fishing" />} />
-          </>
-          )
-        }
+        )}
+        { !logged
+         && (
+           <>
+             <Route path="/shop" element={<NotAuthorize />} />
+             <Route exact path="/inventory" element={<NotAuthorize />} />
+             <Route exact path="/fighting" element={<NotAuthorize />} />
+             <Route path="/craft" element={<NotAuthorize />} />
+             <Route path="/jobs/mining" element={<NotAuthorize job="mining" />} />
+             <Route path="/jobs/fishing" element={<NotAuthorize job="fishing" />} />
+             <Route path="/success" element={<NotAuthorize />} />
+           </>
+         )}
         <Route path="*" element={<NotFound />} />
 
       </Routes>
     </div>
   );
-}
+};
 // == Export
 export default App;
---
+++ Front/src/components/Success/index.js
@@ -0,0 +1,8 @@
+import './style.scss';
+
+export default function Success() {
+  console.log('test');
+  return (
+    <h1>hello</h1>
+  );
+}
